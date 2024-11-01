bf411a13a52f69270eb9000d160aa565d1bfa0b1
Kutub Udd <socialnetwork98710@gmail.com>
2024-11-01 23:02:02 +0600

    added banner height

--- src/app/Components/Hero/Hero.js
+++ src/app/Components/Hero/Hero.js
@@ -3,8 +3,8 @@ import React from "react";
 import hero from "@/app/Assets/Hero.jpg";
 const Hero = () => {
   return (
-    <div className="customWidth px-8 py-6 my-8 h-[320px]">
-      <Image src={hero} width={1320} height={320} alt="dailyNecessities" />
+    <div className="customWidth px-8 py-6 my-8 h-[480px]">
+      <Image src={hero} width={1320} height={480} alt="dailyNecessities" />
     </div>
   );
 };
