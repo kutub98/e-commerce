bd25ff4c88a1222f46781a07e32abea31e45cfb6
Kutub Udd <socialnetwork98710@gmail.com>
2024-11-01 23:37:01 +0600

    added

--- src/app/Components/Hero/Hero.js
+++ src/app/Components/Hero/Hero.js
@@ -3,8 +3,8 @@ import React from "react";
 import hero from "@/app/Assets/Hero.jpg";
 const Hero = () => {
   return (
-    <div className="customWidth px-8 py-6 my-8 h-[480px]">
-      <Image src={hero} width={1320} height={480} alt="dailyNecessities" />
+    <div className="customWidth px-8 py-6 my-8 lg:h-[480px] md:h-[600px] h-[700px]">
+      <Image src={hero} height="100%" width="100%" alt="dailyNecessities" />
     </div>
   );
 };
