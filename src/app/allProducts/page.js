e795d62b396b0d1e9a7293e3d61c12b9ab2200a4
Kutub Udd <socialnetwork98710@gmail.com>
2024-11-03 14:15:09 +0600

    added

--- src/app/allProducts/page.js
+++ src/app/allProducts/page.js
@@ -128,22 +128,29 @@ const AllProducts = () => {
               />
             </h1>
           </div>
-
-          <div
-            className={` transform h-auto p-0 m-0 duration-500 ease-in-out overflow-hidden ${
-              openTabs
-                ? " scale-y-100 h-auto opacity-100"
-                : " scale-y-0 max-h-0 opacity-0 m-0 p-0"
-            }`}
-          >
-            {navLinksWithLink.map((item, key) => (
-              <ul key={key}>
-                <Typography className="px-4 py-2 rounded cursor-pointer capitalize m-1 relative overflow-hidden group">
-                  {item.name}
-                  <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#ff8c42] transition-all duration-300 group-hover:w-full"></span>
-                </Typography>
-              </ul>
-            ))}
+          <div className="flex justify-between">
+            <div
+              className={` justify-center transform h-auto p-0 m-0 duration-500 ease-in-out overflow-hidden ${
+                openTabs
+                  ? " scale-y-100 h-auto opacity-100"
+                  : " scale-y-0 max-h-0 opacity-0 m-0 p-0"
+              }`}
+            >
+              {navLinksWithLink.map((item, key) => (
+                <ul key={key}>
+                  <Typography className="px-4 py-2 rounded cursor-pointer capitalize m-1 relative overflow-hidden group">
+                    {item.name}
+                    <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-[#ff8c42] transition-all duration-300 group-hover:w-full"></span>
+                  </Typography>
+                </ul>
+              ))}
+            </div>
+            <span
+              onClick={() => setOpenFilters(false)}
+              className="text-base md:hidden pr-4"
+            >
+              Close
+            </span>
           </div>
         </div>
       </div>
